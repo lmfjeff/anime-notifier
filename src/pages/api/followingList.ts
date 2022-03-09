454d1a6201ad736ac07b44b601ac584ab6ed4495
lmfjeff <lmfjeff@gmail.com>
2022-03-10 00:12:30 +0800

    rename, fix not show deleted anime in following list

---
+++ src/pages/api/followingList.ts
@@ -0,0 +1,38 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { getSession } from 'next-auth/react'
+import { getAnimesByIds } from '../../services/animeService'
+import { getFollowing } from '../../services/followingService'
+import { FollowingAnime } from '../../types/anime'
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const session = await getSession({ req })
+  const { userId } = session as { userId?: string }
+  if (!session || !userId) return res.status(401).end()
+
+  if (req.method === 'GET') {
+    try {
+      const { animeIds } = await getFollowing({ userId })
+      const { page } = req.query as { page: string }
+      const index = parseInt(page)
+      const animesIdsPortion = animeIds.reverse().slice(index, index + 50)
+      const { animes } = await getAnimesByIds({ animeIds: animesIdsPortion })
+
+      // cope with deleted animes
+      if (animes.length < animesIdsPortion.length) {
+        const retrivedAnimeIds = animes.map(({ id }) => id)
+        const deletedAnimeIds = animesIdsPortion.filter(id => !retrivedAnimeIds.includes(id))
+        deletedAnimeIds.forEach(id => animes.push({ id, title: '[已刪除的動畫]' }))
+      }
+
+      const sortedAnimes = animes.sort(
+        (a: FollowingAnime, b: FollowingAnime) => animeIds.indexOf(a.id) - animeIds.indexOf(b.id)
+      )
+
+      res.status(200).json({ animes: sortedAnimes, total: animeIds.length })
+    } catch (error) {
+      res.status(400).json(error)
+    }
+  }
+
+  res.status(405).end()
+}
