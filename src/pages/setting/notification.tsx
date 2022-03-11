69254b56f9823b873c1050bbd215730ce03bca2e
lmfjeff <lmfjeff@gmail.com>
2022-03-11 12:56:37 +0800

    overhaul notification page, use mutation and optimistic update for changing sub, rely on database sub instead of browser for source of truth

--- src/pages/setting/notification.tsx
+++ src/pages/setting/notification.tsx
@@ -1,100 +1,78 @@
 import { Button } from '@chakra-ui/button'
-import { Box, Flex, Text } from '@chakra-ui/layout'
+import { Box, Flex, Text, VStack } from '@chakra-ui/layout'
+import { Skeleton } from '@chakra-ui/react'
 import { Spinner } from '@chakra-ui/spinner'
 import { Switch } from '@chakra-ui/switch'
 import axios from 'axios'
 import { GetServerSideProps } from 'next'
-import { getSession, useSession } from 'next-auth/client'
+import { getSession, useSession } from 'next-auth/react'
 import React, { useCallback, useEffect, useState } from 'react'
+import { useMutation, useQuery, useQueryClient } from 'react-query'
 import { HtmlHead } from '../../components/HtmlHead'
-import { SettingPanel } from '../../components/SettingPanel'
+import { SettingPanelTab } from '../../components/SettingPanelTab'
 import * as swHelper from '../../utils/swHelper'
+
+export const getServerSideProps: GetServerSideProps = async context => {
+  const session = await getSession(context)
+  return {
+    props: { session },
+  }
+}
 
 Notification.getTitle = '設定'
 
 export default function Notification() {
-  const [session] = useSession()
+  const { data: session } = useSession()
+  const queryClient = useQueryClient()
 
-  const [isSubscribed, setIsSubscribed] = useState(false)
-  const [sub, setSub] = useState<any>(null)
-  const [loading, setLoading] = useState(false)
-  const [initializing, setInitializing] = useState(true)
+  const [loadingToggle, setLoadingToggle] = useState(false)
 
-  const updateSub = async (sub: string) => {
-    const resp = await axios.post('/api/setting', { sub })
-    console.log(resp)
+  const fetchSub = async () => {
+    const { data } = await axios.get('/api/setting')
+    return data?.sub
   }
 
-  useEffect(() => {
-    async function checkSub() {
-      const subscription = await swHelper.checkSubscription()
-      setIsSubscribed(!!subscription)
-      setSub(subscription || null)
-      // const resp = await axios.get('/api/setting')
-      // console.log(resp.data?.sub)
-      setInitializing(false)
-    }
-    checkSub()
-  }, [])
+  const { data: sub, isLoading: loadingGetSub } = useQuery('getSub', fetchSub, { enabled: !!session })
 
-  const handleToggle = useCallback(async () => {
-    setLoading(true)
-    if (isSubscribed) {
+  const mutateSub = useMutation(
+    async sub => {
+      const { data } = await axios.post('/api/setting', { sub })
+      return data?.sub || null
+    },
+    {
+      onMutate: async (sub: string | null) => {
+        await queryClient.cancelQueries('getSub')
+        const previousSub = queryClient.getQueryData('getSub')
+        queryClient.setQueryData('getSub', sub)
+        return { previousSub }
+      },
+      onSuccess: (sub, variables, context) => {
+        queryClient.setQueryData('getSub', sub)
+      },
+      onError: (error, variables, context) => {
+        if (context?.previousSub) {
+          queryClient.setQueryData('getSub', context.previousSub)
+        }
+      },
+    }
+  )
+
+  const handleToggle = async () => {
+    setLoadingToggle(true)
+    if (sub) {
       await swHelper.unsubscribe()
-      await updateSub('')
-      setIsSubscribed(false)
-      setSub(null)
+      mutateSub.mutate(null)
     } else {
-      const sub = await swHelper.subscribe()
-      await updateSub(JSON.stringify(sub))
-      setIsSubscribed(true)
-      setSub(sub)
+      const newSub = await swHelper.subscribe()
+      if (newSub) {
+        mutateSub.mutate(JSON.stringify(newSub))
+      }
     }
-    setLoading(false)
-  }, [isSubscribed])
-
-  const sendTestNotification = async (event: any) => {
-    event.preventDefault()
-    await swHelper.testNotification()
+    setLoadingToggle(false)
   }
 
-  const NotificationPanel = () => {
-    return (
-      <>
-        <HtmlHead title="設定" />
-        <Flex justifyContent="center" alignItems="center">
-          <Flex flexDir="column" w="300px" p={5}>
-            {!initializing ? (
-              <>
-                <Box mb={5}>
-                  <Text mb={2}>允許推送通知:</Text>
-                  <Switch
-                    aria-label="Enable Notification"
-                    // defaultChecked={isSubscribed}
-                    isChecked={isSubscribed}
-                    onChange={() => {
-                      handleToggle()
-                    }}
-                    isDisabled={loading}
-                  />
-                  {loading && <Spinner />}
-                </Box>
-                {/* <Button onClick={() => console.log(sub)}>see sub</Button>
-                <Button onClick={() => console.log(isSubscribed)}>see issub</Button> */}
-                <Box mb={5}>
-                  <Text mb={2}>測試:</Text>
-                  <Button onClick={sendTestNotification} colorScheme="blue" isDisabled={!isSubscribed}>
-                    發出測試通知
-                  </Button>
-                </Box>
-              </>
-            ) : (
-              <Spinner />
-            )}
-          </Flex>
-        </Flex>
-      </>
-    )
+  const sendTestNotification = async () => {
+    await axios.post('/api/notification')
   }
 
   if (!session)
@@ -105,15 +83,26 @@ export default function Notification() {
     )
 
   return (
-    <SettingPanel>
-      <NotificationPanel />
-    </SettingPanel>
+    <>
+      <HtmlHead title="設定" />
+      <SettingPanelTab />
+      <Flex flexDir="column" alignItems={'center'}>
+        <Skeleton isLoaded={!loadingGetSub} borderRadius={5}>
+          <Flex w="250px" flexDir="column" gap={2}>
+            <Text>允許推送通知:</Text>
+            <Flex gap={3} align={'center'}>
+              <div>
+                <Switch isChecked={!!sub} onChange={() => handleToggle()} isDisabled={loadingToggle} />
+              </div>
+              {loadingToggle && <Spinner size={'sm'} />}
+            </Flex>
+            <Text>測試:</Text>
+            <Button onClick={() => sendTestNotification()} colorScheme="blue" isDisabled={!sub}>
+              發出測試通知
+            </Button>
+          </Flex>
+        </Skeleton>
+      </Flex>
+    </>
   )
-}
-
-export const getServerSideProps: GetServerSideProps = async context => {
-  const session = await getSession(context)
-  return {
-    props: { session },
-  }
 }
