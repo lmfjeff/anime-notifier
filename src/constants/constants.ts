a0adb50d69d2e52179dc7cb92e8a36612302782c
Jeff Lau Ming Fung <jefflau@nmg.com.hk>
2023-11-14 00:33:10 +0800

    format

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,3 +1,3 @@
-export const MAL_ANIME_PAGE_ID_URL='https://myanimelist.net/anime/'
+export const MAL_ANIME_PAGE_ID_URL = 'https://myanimelist.net/anime/'
 
-export const ANILIST_ANIME_PAGE_ID_URL='https://anilist.co/anime/'
+export const ANILIST_ANIME_PAGE_ID_URL = 'https://anilist.co/anime/'
